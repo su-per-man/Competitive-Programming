7f239f3fe626313f7e8644ca610b1432a091d2b7
Suman Kumar <suman98kumar@gmail.com>
2020-08-21 23:52:59 +0530

    CHEFWED

--- CodeChef/August Challenge 2020 Division 2/CHEFWED.java
+++ CodeChef/August Challenge 2020 Division 2/CHEFWED.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Codechef
+class CHEFWED
 {
     static int[] arr, cache;
     static int n, k;
